b9601d04f5076f26e7f68dd6ab36029aeac57656
rnihesh <niheshr03@gmail.com>
2025-01-31 23:51:02 +0530

    fix await

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,7 +255,7 @@ const updateAccountDetails = asyncHandler(async (ree, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
